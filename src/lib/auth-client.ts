3d2d3f859ad2d54628e64a94d205cb795dac19f4
deadlord082 <135032460+deadlord082@users.noreply.github.com>
2025-11-13 15:07:00 +0100

    added github oauth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,2 +1,9 @@
 import { createAuthClient } from 'better-auth/react'
 export const authClient = createAuthClient()
+
+export const signInWithGithub = async () => {
+  await authClient.signIn.social({
+    provider: 'github',
+    callbackURL: '/app'
+  })
+}
